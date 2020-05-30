aecf1a5d532adadc47dd50238146ce5543191f2e
HoangDangKhanh <hoangdangkhanh12c1@gmail.com>
2020-05-30 10:46:31 +0700

    started 2

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, function() {
+    console.log('Running on port: ' + PORT)
+})
+
+app.get('/', function(req, res) {
+    res.send('Hi, i\'m a chatbot');
+})
+app.get('/webhook/', function(req, res) {
+    if (req.query['hub.verify_token'] === 'nhanvanchatbot') {
+        res.send(req.query['hub.verify_token']);
+    }
+    res.send('Wrong token');
+})
