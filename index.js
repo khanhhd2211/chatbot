f40d5088f2118cf483c7c66c6c34a658a0dcfdd3
HoangDangKhanh <hoangdangkhanh12c1@gmail.com>
2020-05-30 10:58:15 +0700

    fix heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function() {
     console.log('Running on port: ' + PORT)
