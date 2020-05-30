554c2cb940bea8c50ebd3b03f7a9c52f87da4c45
HoangDangKhanh <hoangdangkhanh12c1@gmail.com>
2020-05-30 11:45:16 +0700

    chatbot

--- index.js
+++ index.js
@@ -16,3 +16,23 @@ app.get('/webhook/', function(req, res) {
     }
     res.send('Wrong token');
 })
+app.post('/webhook/', function(req, res) {
+    let messaging_events = req.body.entry[0].messaging_events
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = messaging_events[i];
+        let sender = event.sender.id;
+        if (event.message && event.message.text) {
+            let text = event.message.text;
+            sendText(sender, `Text echo ${text.substring(0, 100)}`)
+        }
+    }
+    res.sendStatus(200)
+})
+
+function sendText(sender, text) {
+    let messageData = {text}
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: {access_token: "EAAEvpJTMNgkBAFRxqM2nmIi4mrHsq6PW2xqwpqfQJHb08BkC4NN3baBAUcw8KS8PtiTEFiul4SZC2oyvPmZBBcqrGqdZAFdR2Mj83NZAWwvTj5jZCN6hlVwdEPoYxcAA3nqmpI20Wm5BU9GFVZC51Ik6hHNH3qX0tIxh1lHaC97y1eARVHZCQYT"}
+    })
+}
