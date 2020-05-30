a37031204f85a96dd4cc5ffc7b69c89fdbb6f3c1
HoangDangKhanh <hoangdangkhanh12c1@gmail.com>
2020-05-30 22:14:16 +0700

    chatbot

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ var token = "EAAEmJVLT904BANWTCOXOUrZCmZC3R6sZCewbpQDBHmXFLuFyUA48wIzVZC0kPdc1TC
 async function checkDay() {
   var gym = await daysAtGym.find()
   var { daysAtGym: days } = gym;
-  sendTextMessage(sender, `Bạn đã tập được ${gym} ngày`)
+  sendTextMessage(sender, `Bạn đã tập được ${gym.daysAtGym} ngày`)
 }
 
 function sendTextMessage(sender, text) {
