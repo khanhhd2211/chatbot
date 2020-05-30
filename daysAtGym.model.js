7065bb4c25f54aa4e3c1710c346c4bdf2c0cb69e
HoangDangKhanh <hoangdangkhanh12c1@gmail.com>
2020-05-30 22:20:02 +0700

    chatbot

--- daysAtGym.model.js
+++ daysAtGym.model.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var DaysAtGymSchema = new mongoose.Schema({
-	daysAtGym: String
+	daysAtGym: Number
 })
 
 var DaysAtGym = mongoose.model('DaysAtGym', DaysAtGymSchema, 'day');
