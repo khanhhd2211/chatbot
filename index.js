ba2709917352d31dfc7343ce21c64debe8d432dc
HoangDangKhanh <hoangdangkhanh12c1@gmail.com>
2020-05-30 18:06:03 +0700

    chat bot

--- index.js
+++ index.js
@@ -1,69 +1,30 @@
-const APP_SECRET = '835adfad5b3dcfdb256be78b1459d9f7';
-const VALIDATION_TOKEN = 'khanhchatbot';
-const PAGE_ACCESS_TOKEN = 'EAAEvpJTMNgkBABVBaEwc4VycQgZBpiwga84owz8TeEGG9ksnnpbWLnsvsg34wynmHJrM9KL5R2BNk4cGsirW7DdVAcpZCU13MfLUUqDZCXWP48tKudlvFaK6RZBkOYZA5bA47yQNeYXwUYij11yBZB1qC8i6mxr1LvQmQo5Sl2naKVTtZBhe2UG';
-
-var http = require('http');
-var bodyParser = require('body-parser');
-var express = require('express');
-
-var app = express();
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-var server = http.createServer(app);
-var request = require("request");
-
-app.get('/', (req, res) => {
-  res.send("Home page. Server running okay.");
-});
-
-app.get('/webhook', function(req, res) { // Đây là path để validate tooken bên app facebook gửi qua
-  if (req.query['hub.verify_token'] === VALIDATION_TOKEN) {
-    res.send(req.query['hub.challenge']);
-  }
-  res.send('Error, wrong validation token');
-});
-
-app.post('/webhook', function(req, res) { // Phần sử lý tin nhắn của người dùng gửi đến
-  console.log(req.body.entry);
-  var entries = req.body.entry;
-  for (var entry of entries) {
-    var messaging = entry.messaging;
-    for (var message of messaging) {
-      var senderId = message.sender.id;
-      if (message.message) {
-        if (message.message.text) {
-          var text = message.message.text;
-          sendMessage(senderId, "Hello!! I'm a bot. Your message: " + text);
-        }
-      }
-    }
-  }
-  res.status(200).send("OK");
-});
-
-// Đây là function dùng api của facebook để gửi tin nhắn
-function sendMessage(senderId, message) {
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {
-      access_token: PAGE_ACCESS_TOKEN,
-    },
-    method: 'POST',
-    json: {
-      recipient: {
-        id: senderId
-      },
-      message: {
-        text: message
-      },
-    }
-  });
-}
-
-app.set('port', process.env.PORT || 5000);
-app.set('ip', process.env.IP || "0.0.0.0");
-
-server.listen(app.get('port'), app.get('ip'), function() {
-  console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
-});
+var express = require('express')
+ var bodyParser = require('body-parser')
+ var request = require('request')
+ var app = express()
+ 
+ app.set('port', (process.env.PORT || 5000))
+ 
+ // Process application/x-www-form-urlencoded
+ app.use(bodyParser.urlencoded({extended: false}))
+ 
+ // Process application/json
+ app.use(bodyParser.json())
+ 
+ // Index route
+ app.get('/', function (req, res) {
+     res.send('Hello world, I am a chat bot')
+ })
+ 
+ // for Facebook verification
+ app.get('/webhook/', function (req, res) {
+     if (req.query['hub.verify_token'] === 'khanhchatbot') {
+         res.send(req.query['hub.challenge'])
+     }
+     res.send('Error, wrong token')
+ })
+ 
+ // Spin up the server
+ app.listen(app.get('port'), function() {
+     console.log('running on port', app.get('port'))
+ })
